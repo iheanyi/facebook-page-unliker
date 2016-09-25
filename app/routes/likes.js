306034f00061bb8c21c58b9f26549ea8b122223b
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2016-09-25 14:40:53 -0400

    Get initial fetching of the pages complete.

--- app/routes/likes.js
+++ app/routes/likes.js
@@ -1,5 +1,45 @@
 import Ember from 'ember';
 
+function transformResponse(data) {
+  return Ember.$.map(data, (item) => {
+    return {
+      type: "likes",
+      id: item.id,
+      attributes: {
+        name: item.name,
+        'created-time': item.created_time
+      }
+    };
+  });
+}
+
 export default Ember.Route.extend({
-  session: Ember.inject.service()
+  session: Ember.inject.service(),
+  baseAPIUrl: Ember.computed('session', function() {
+    const accessToken = this.get('session.session.authenticated.accessToken');
+    return `https://graph.facebook.com/me/likes?access_token=${accessToken}&limit=100`;
+  }),
+  getLikesFromUrl(url) {
+    return Ember.$.ajax(url).then((response) => {
+      if (response.paging && response.paging.next) {
+        const transformedLikes = transformResponse(response.data);
+        const payload = {
+          data: transformedLikes
+        };
+
+        this.store.pushPayload('like', payload);
+
+        const nextUrl = response.paging.next;
+
+        return this.getLikesFromUrl(nextUrl);
+      } else {
+        return this.store.peekAll('like');
+      }
+    });
+  },
+  model() {
+    const baseApiUrl = this.get('baseAPIUrl');
+
+    return this.getLikesFromUrl(baseApiUrl);
+  }
 });

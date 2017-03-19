90e1266627fc47abb1f239d21f7972011beae8e6
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2017-03-19 00:53:33 -0400

    Add in a mixin for the application route.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,8 @@
 /* globals FB */
 import Ember from 'ember';
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ApplicationRouteMixin, {
   session: Ember.inject.service('session'),
   beforeModel() {
     if (this.get('session.isAuthenticated')) {

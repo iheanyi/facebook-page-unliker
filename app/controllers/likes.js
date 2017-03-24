46ebe12dd4046105699aa4ad85e71cf9f8291f8f
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2017-03-24 17:59:47 -0400

    Fix up pagination on the page rather than the jank stuff we had before.

--- app/controllers/likes.js
+++ app/controllers/likes.js
@@ -1,12 +1,26 @@
 import Ember from 'ember';
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 export default Ember.Controller.extend({
   likesArr: [],
   allLikes: [],
   currentLikesIndex: 0,
+  page: 1,
+  perPage: 25,
+  scrollTop: Ember.observer("page", function() {
+    Ember.$(window).scrollTop(0);
+  }),
+  reversedLikes: Ember.computed('model', function() {
+    return this.get('model').reverse();
+  }),
+  totalPages: Ember.computed.oneWay("pagedContent.totalPages"),
   currentLikesArr: Ember.computed('currentLikesIndex', 'likesArr', function() {
     let idx = this.get('currentLikesIndex');
     return this.get('likesArr').reverse()[idx];
+  }),
+  pagedContent: pagedArray('reversedLikes', {
+    page: Ember.computed.alias("parent.page"),
+    perPage: Ember.computed.alias("parent.perPage")
   }),
   actions: {
     setCurrentPage(index) {
